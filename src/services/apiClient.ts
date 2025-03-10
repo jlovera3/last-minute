208932f2bcd0c7d7da014b778eaece68baf8e854
jlovera3 <jloveraulecia@gmail.com>
2025-03-10 18:12:51 +0100

    chore: adding environment files

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -1,10 +1,22 @@
 import Constants from "expo-constants";
+import { API_URL } from "@env"; // Import the API_URL from the correct .env file
+
+const getApiUrl = () => {
+  if (__DEV__) {
+    return API_URL; // Development mode
+  }
+
+  return Constants.expoConfig?.extra?.ENV === "production"
+    ? API_URL // Production URL
+    : API_URL; // Integration URL
+};
 
 /**
- * Base API URL from the environment variables.
+ * Base API URL from environment variables.
  * @type {string}
  */
-const API_URL: string = Constants.expoConfig?.extra?.API_URL || process.env.API_URL;
+const API_URL_FINAL = getApiUrl();
+console.log("🚀 ~ API_URL_FINAL:", API_URL_FINAL);
 
 /**
  * Generic function to handle API requests.
@@ -14,17 +26,12 @@ const API_URL: string = Constants.expoConfig?.extra?.API_URL || process.env.API_
  */
 export async function apiRequest<T>(endpoint: string, options: RequestInit = {}): Promise<T> {
     try {
-        const response = await fetch(`${API_URL}${endpoint}`, {
-            headers: { "Content-Type": "application/json" },
-            ...options,
-            mode: "no-cors",
-        });
-        console.log("🚀 ~ response:", response);
+        const response = await fetch(`${API_URL_FINAL}${endpoint}`, options);
+        console.log("response:", response);
 
         switch (response.status) {
             case 200: {
                 const data = await response.json();
-                console.log("🚀 ~ data:", data);
                 return data;
             }
             case 400:
